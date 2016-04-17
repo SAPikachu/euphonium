35baf414c68056d47b35ff6413cc40dfc3fdb7f9
SAPikachu <sapikachu@gmail.com>
2016-04-17 20:52:01 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+extern crate env_logger;
+extern crate mioco;
+extern crate trust_dns;
+extern crate rand;
+#[macro_use] extern crate quick_error;
+#[macro_use] extern crate log;
+
+mod utils;
+
+use std::net::{SocketAddr, SocketAddrV4, IpAddr};
+
+use mioco::udp::UdpSocket;
+use mioco::mio::Ipv4Addr;
+use trust_dns::rr::domain;
+use trust_dns::rr::{DNSClass, RecordType, Record, RData};
+use trust_dns::op::{Message, MessageType, ResponseCode, Query, OpCode};
+use trust_dns::serialize::binary::{BinDecoder, BinEncoder, BinSerializable};
+
+use utils::{Result, CloneExt, MessageExt};
+
+fn query(q: Query, server: Ipv4Addr) -> Result<Message> {
+    debug!("Q[{}][{}] {:?} {:?}",
+           server, q.get_name(), q.get_query_type(), q.get_query_class());
+    let mut msg : Message = Message::new();
+    msg.message_type(MessageType::Query);
+    msg.id(rand::random());
+    msg.op_code(OpCode::Query);
+    msg.recursion_desired(true);
+    msg.add_query(q);
+    let msg_data = try!(msg.to_bytes());
+    let mut sock = try!(UdpSocket::v4());
+    let target = SocketAddr::new(IpAddr::V4(server), 53);
+    try!(sock.send(&msg_data, &target));
+    loop {
+        let (resp, addr) = try!(Message::from_udp(&mut sock));
+        if addr != target {
+            warn!("Q[{}][{}] Response from unexpected server: {}",
+                  server, msg.get_queries()[0].get_name(), addr);
+            continue
+        }
+        if ! resp.is_resp_for(&msg) {
+            warn!("Q[{}][{}] Invalid response: {:?}",
+                  server, msg.get_queries()[0].get_name(), resp);
+            continue
+        }
+        // TODO: Validate response, retry and timeout
+        return Ok(resp);
+    };
+}
+
+fn handle_request(msg: Message) -> Message {
+    let mut ret : Message = msg.new_resp();
+    ret.recursion_available(true);
+    // TODO: EDNS?
+    match query(msg.get_queries()[0].clone(), Ipv4Addr::new(8, 8, 8, 8)) {
+        Ok(resp) => { ret.copy_resp_from(&resp); },
+        Err(_)   => { ret.response_code(ResponseCode::ServFail); },
+    };
+    ret
+}
+
+fn main() {
+    env_logger::init().unwrap();
+    println!("Hello, world!");
+
+    mioco::start(move || {
+        let ip = Ipv4Addr::new(0, 0, 0, 0);
+        let port = 5354;
+        let addr = SocketAddr::V4(SocketAddrV4::new(ip, port));
+
+        mioco::spawn(move || -> Result<()> {
+            let mut sock : UdpSocket = UdpSocket::v4().unwrap();
+            sock.bind(&addr).unwrap();
+
+            loop {
+                let (msg, addr) = try!(Message::from_udp(&mut sock));
+                let ret = handle_request(msg);
+                let resp = try!(ret.to_bytes());
+                try!(sock.send(&resp, &addr));
+            }
+        });
+    }).unwrap();
+}
---
+++ src/utils.rs
@@ -0,0 +1,97 @@
+use std;
+use std::io;
+use std::convert::From;
+
+use std::net::SocketAddr;
+
+use mioco::udp::UdpSocket;
+use trust_dns::op::{Message, Query, MessageType};
+use trust_dns::error::{DecodeError, EncodeError};
+use trust_dns::serialize::binary::{BinDecoder, BinEncoder, BinSerializable};
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum Error {
+        Io(err: io::Error) {
+            from()
+            description(err.description())
+        }
+        Decode(err: DecodeError) {
+            from()
+            description(err.description())
+        }
+        Encode(err: EncodeError) {
+            from()
+            description(err.description())
+        }
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub trait CloneExt {
+    fn clone(&self) -> Self;
+}
+
+impl CloneExt for Query {
+    fn clone(&self) -> Query {
+        let mut ret = Query::new();
+        ret.name(self.get_name().clone())
+            .query_type(self.get_query_type())
+            .query_class(self.get_query_class());
+        ret
+    }
+}
+
+pub trait MessageExt {
+    fn to_bytes(&self) -> Result<Vec<u8>>;
+    fn from_bytes(buf: &[u8]) -> Result<Message>;
+    fn from_udp(sock: &mut UdpSocket) -> Result<(Message, SocketAddr)>;
+    fn new_resp(&self) -> Message;
+    fn copy_resp_from(&mut self, other: &Message);
+    fn is_resp_for(&self, other: &Message) -> bool;
+}
+impl MessageExt for Message {
+    fn to_bytes(&self) -> Result<Vec<u8>> {
+        let mut ret = Vec::<u8>::new();
+        {
+            let mut encoder = BinEncoder::new(&mut ret);
+            try!(self.emit(&mut encoder));
+        }
+        Ok(ret)
+    }
+    fn from_bytes(buf: &[u8]) -> Result<Message> {
+        let mut decoder = BinDecoder::new(buf);
+        let msg = try!(Message::read(&mut decoder));
+        Ok(msg)
+    }
+    fn from_udp(sock: &mut UdpSocket) -> Result<(Message, SocketAddr)> {
+        let mut buf = [0u8; 1024 * 16];
+        let (l, addr) = try!(sock.recv(&mut buf));
+        let msg = try!(Message::from_bytes(&buf[0..l]));
+        Ok((msg, addr))
+    }
+    fn new_resp(&self) -> Message {
+        let mut ret : Message = Message::new();
+        ret.message_type(MessageType::Response);
+        ret.id(self.get_id());
+        ret.op_code(self.get_op_code());
+        ret.recursion_desired(self.is_recursion_desired());
+        for q in self.get_queries() {
+            ret.add_query(q.clone());
+        }
+        ret
+    }
+    fn copy_resp_from(&mut self, other: &Message) {
+        self.response_code(other.get_response_code());
+        self.add_all_answers(other.get_answers());
+        self.add_all_name_servers(other.get_name_servers());
+        for rec in other.get_additional() {
+            self.add_additional(rec.clone());
+        }
+    }
+    fn is_resp_for(&self, other: &Message) -> bool {
+        // TODO
+        true
+    }
+}
